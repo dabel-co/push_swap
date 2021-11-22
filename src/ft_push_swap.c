6d4513a72bcdff38bf6c75d2694b403ae442403d
dabel-co <dabel-co@c1r9s6.42madrid.com>
2021-11-22 12:36:27 +0100

    fixed too many operations for short sorting

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -6,11 +6,24 @@
 /*   By: dabel-co <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 11:00:32 by dabel-co          #+#    #+#             */
-/*   Updated: 2021/11/16 15:52:05 by dabel-co         ###   ########.fr       */
+/*   Updated: 2021/11/22 12:35:48 by dabel-co         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static t_list	*ft_lst_add_element(int content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = malloc(sizeof(int));
+	new->next = NULL;
+	*(int *)new->content = content;
+	return (new);
+}
 
 static	t_list	*ft_fill(char *argv, t_list *stack)
 {
@@ -24,7 +37,7 @@ static	t_list	*ft_fill(char *argv, t_list *stack)
 			printf("bad parse\n");
 			exit(0);
 		}
-		ft_lstadd_back(&stack, ft_lstnew((void *)(long)p));
+		ft_lstadd_back(&stack, ft_lst_add_element(p));
 		if (ft_lst_repeat(stack) == -1)
 				printf("SAME\n");
 		while (ft_isnumber(*argv) && *argv != ' ' && *argv != '\0')
@@ -38,24 +51,19 @@ void	checking(t_list **a, t_list **b)
 {
 	t_list *x = *a;
 	t_list *y = *b;
-	int d = 0;
 	printf("STACK_A\n");
 	while (x != NULL)
 	{
-		d = (int)x->content;
 		usleep(100000);
-		printf("%d\n", x->content);
-	//	printf("%p\n", x->next);
+		printf("%d\n", *(int *)x->content);
 		x = x->next;
 	}
 	if (y != NULL)
 		printf("STACK_B\n");
 	while (y != NULL)
 	{
-		d = (int)y->content;
 		usleep(100000);
-		printf("%d\n", y->content);
-	//	printf("%p\n", y->next);
+		printf("%d\n", *(int *)y->content);
 		y = y->next;
 	}
 }
@@ -71,6 +79,8 @@ int	main(int argc, char **argv)
 	while (i++ < argc && argv++)
 		a = ft_fill(*argv, a);
 	if (ft_lstsize(a) <= 5)
-		ft_small_short(&a, &b);	
+		ft_small_short(&a, &b);
+	//else
+		//ft_big_short(&a, &b);
 	checking (&a, &b);
 }
--- src/ft_short_sorting.c
+++ src/ft_short_sorting.c
@@ -6,50 +6,73 @@
 /*   By: dabel-co <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 17:51:53 by dabel-co          #+#    #+#             */
-/*   Updated: 2021/11/16 15:52:07 by dabel-co         ###   ########.fr       */
+/*   Updated: 2021/11/22 12:35:45 by dabel-co         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-static void	ft_short_five(t_list **a, t_list **b)
+/*This function will find the position of the smaller element in the list*/
+static int	ft_find_smaller(t_list **a)
 {
-	ft_push(a, b, 'a');
-	if ((*a)->content > (*a)->next->next->next->content)
-		ft_rotate(a, b, 'a');
-	if ((*a)->content > (*a)->next->next->content && (*a)->content <
-			(*a)->next->next->next->content)
-		ft_rotate_rev(a, b, 'a');
-	if ((*a)->content > (*a)->next->content)
-		ft_swap(a, b, 'a');
-	if (ft_lst_order(*a) == -1)
+	int		i;
+	int		smaller;
+	t_list	*aux;
+	int		r;
+
+	r = 0;
+	aux = *a;
+	smaller = *(int *)aux->content;
+	i = 0;
+	while (aux != NULL)
 	{
-		ft_rotate(a, b ,'a');
-		ft_rotate(a, b ,'a');
+		if (smaller > *(int *)aux->content)
+		{
+			i = r;
+			smaller = *(int *)aux->content;
+		}
+		r++;
+		aux = aux->next;
 	}
+	return (i);
+}
+
+static void	ft_move(t_list **a, t_list **b)
+{
+	if (ft_find_smaller(a) == 1)
+		ft_swap(a, b, 'a');
+	if (ft_find_smaller(a) == 2)
+	{
+		ft_rotate(a, b, 'a');
+		ft_rotate(a, b, 'a');
+	}
+	if (ft_find_smaller(a) == 3)
+		ft_rotate_rev(a, b, 'a');
+	if (ft_find_smaller(a) == 4 || (ft_find_smaller(a) == 3
+			&& ft_lstsize(*a) == 5))
+		ft_rotate_rev(a, b, 'a');
+	ft_push(a, b, 'b');
 }
 
 void	ft_small_short(t_list **a, t_list **b)
 {
 	while (ft_lstsize(*a) > 3)
-		ft_push(a, b, 'b');
-	if ((*a)->content > (*a)->next->content &&
-			(*a)->content < (*a)->next->next->content)
+		ft_move(a, b);
+	if (*(int *)(*a)->content > *(int *)(*a)->next->content
+		&& *(int *)(*a)->content < *(int *)(*a)->next->next->content)
 		ft_swap(a, b, 'a');
-	if ((*a)->content > (*a)->next->content &&
-			(*a)->next->content > (*a)->next->next->content)
+	if (*(int *)(*a)->content > *(int *)(*a)->next->content
+		&& *(int *)(*a)->next->content > *(int *)(*a)->next->next->content)
 		ft_swap(a, b, 'a');
-	if ((*a)->content < (*a)->next->content &&
-			(*a)->content < (*a)->next->next->content &&
-			(*a)->next->content > (*a)->next->next->content)
+	if (*(int *)(*a)->content < *(int *)(*a)->next->content
+		&& *(int *)(*a)->content < *(int *)(*a)->next->next->content
+		&& *(int *)(*a)->next->content > *(int *)(*a)->next->next->content)
 		ft_swap(a, b, 'a');
-	if ((*a)->content < (*a)->next->content &&
-			(*a)->content > (*a)->next->next->content)
+	if (*(int *)(*a)->content < *(int *)(*a)->next->content
+		&& *(int *)(*a)->content > *(int *)(*a)->next->next->content)
 		ft_rotate_rev(a, b, 'a');
-	if ((*a)->content > (*a)->next->content &&
-			(*a)->next->content < (*a)->next->next->content)
+	if (*(int *)(*a)->content > *(int *)(*a)->next->content
+		&& *(int *)(*a)->next->content < *(int *)(*a)->next->next->content)
 		ft_rotate(a, b, 'a');
-	if (ft_lstsize(*b) != 0)
-		ft_short_five(a, b);
+	while (ft_lstsize(*b) != 0)
+		ft_push(a, b, 'a');
 }
-
